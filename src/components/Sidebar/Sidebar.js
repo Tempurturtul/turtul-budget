2b287cb11a471cc20842d9409fd5347596f4f869
Tempurturtul <tempurturtul@gmail.com>
2016-12-08 14:16:12 -0500

    Replace default app with WiP Sidebar

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="Sidebar">
+        <h1 className="Sidebar-title">Turtul Budget</h1>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
